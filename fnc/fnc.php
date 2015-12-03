ed9b3a23d4c9c458322433252fac6e581fc02a04
Francisco Alba Ponce <yo@falbaponce.es>
2015-12-03 11:17:37 +0100

    creada funcion create_table_PDO()  es igual que  create_table() pero para usar los resultados traidos con PDO

--- fnc/fnc.php
+++ fnc/fnc.php
@@ -51,5 +51,50 @@
         $table.= '</table>';
         return $table;
     }
+    /**
+     * @param mysqli $result  le pasamos el resultado de la consulta de la base de datos realizada con PDO.
+     * @param array $col_names, array de strings que tiene como valor por defecto un array vacio, si está vacio los nombres de las columnas serán los nombres de las columnas de la base de datos, si queremos darles nombres mas representativos podemos rellenar este campo (ej['nombre','apellido','email'])
+     * @return string $table devuelve el string formado que contiene la tabla.
+     */
+    
+       function create_table_PDO($result,$col_names=array())
+    {
+        $table= '';
+        $rows=array();
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+            array_push($rows,$row);
+        }
+        $table.= '<table>';
+        $table.= '<tr>';
+        if(count($col_names)!== 0){
+            for($i=0 ;$i < count($col_names) ; $i++){
+                $table.= '<th>'.$col_names[$i].'</th>';
+            }
+        }
+          else
+        {
+            foreach($rows as $row){
+                $count=0;
+                foreach($row as $key => $value){
+                    $table.= ' <th>'.$key.'</th>';
+                    if($count > count($row)){
+                        break;
+                    }
+                    $count++;
+                }
+                break;
+            }
+        }
+        $table.='</tr>';
+        foreach($rows as $row){
+            $table.='<tr>';
+            foreach($row as $key => $value){
+                $table.= ' <td>'.$value.'</td>';
+            }
+            $table.='</tr>';
+        }
+        $table.= '</table>';
+        return $table;
+    }
 
  ?>
